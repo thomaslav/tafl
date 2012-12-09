cafa27884bb01f1a36065ae365f575ef91f7116d
Jens Jahnke <jan0sch@gmx.net>
2012-12-09 08:34:26 +0100

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tafl::Application.config.secret_token = 'e5617fe7130e76ce0d5b5ac990de99e95fc9d77d9fefc45d1b665d3a21a9be1e1e6b7edcc9dcee6ee841e98162b783a0a82e0e9c8931c7baac83d632b0210fde'
